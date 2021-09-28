e5f36861dfd677f14101d04e616f190479173e50
theOldalex <alexandrediakitepro@gmail.com>
2021-09-28 10:46:09 +0200

    premier commit

---
+++ mon_site/src/Entity/Z5600.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\Z5600Repository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=Z5600Repository::class)
+ */
+class Z5600
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=11)
+     */
+    private $Rames;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $motrices;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $mise_en_service;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $livree;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nombre_de_caisses;
+
+    /**
+     * @ORM\Column(type="string", length=11)
+     */
+    private $stf;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $radiation;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $equipements_interieurs;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lignes;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRames(): ?string
+    {
+        return $this->Rames;
+    }
+
+    public function setRames(string $Rames): self
+    {
+        $this->Rames = $Rames;
+
+        return $this;
+    }
+
+    public function getMotrices(): ?string
+    {
+        return $this->motrices;
+    }
+
+    public function setMotrices(string $motrices): self
+    {
+        $this->motrices = $motrices;
+
+        return $this;
+    }
+
+    public function getMiseEnService(): ?\DateTimeInterface
+    {
+        return $this->mise_en_service;
+    }
+
+    public function setMiseEnService(\DateTimeInterface $mise_en_service): self
+    {
+        $this->mise_en_service = $mise_en_service;
+
+        return $this;
+    }
+
+    public function getLivree(): ?string
+    {
+        return $this->livree;
+    }
+
+    public function setLivree(string $livree): self
+    {
+        $this->livree = $livree;
+
+        return $this;
+    }
+
+    public function getNombreDeCaisses(): ?int
+    {
+        return $this->nombre_de_caisses;
+    }
+
+    public function setNombreDeCaisses(int $nombre_de_caisses): self
+    {
+        $this->nombre_de_caisses = $nombre_de_caisses;
+
+        return $this;
+    }
+
+    public function getStf(): ?string
+    {
+        return $this->stf;
+    }
+
+    public function setStf(string $stf): self
+    {
+        $this->stf = $stf;
+
+        return $this;
+    }
+
+    public function getRadiation(): ?\DateTimeInterface
+    {
+        return $this->radiation;
+    }
+
+    public function setRadiation(\DateTimeInterface $radiation): self
+    {
+        $this->radiation = $radiation;
+
+        return $this;
+    }
+
+    public function getEquipementsInterieurs(): ?string
+    {
+        return $this->equipements_interieurs;
+    }
+
+    public function setEquipementsInterieurs(string $equipements_interieurs): self
+    {
+        $this->equipements_interieurs = $equipements_interieurs;
+
+        return $this;
+    }
+
+    public function getLignes(): ?string
+    {
+        return $this->lignes;
+    }
+
+    public function setLignes(string $lignes): self
+    {
+        $this->lignes = $lignes;
+
+        return $this;
+    }
+}
---
+++ mon_site/src/Repository/Z5600Repository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Z5600;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Z5600|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Z5600|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Z5600[]    findAll()
+ * @method Z5600[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class Z5600Repository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Z5600::class);
+    }
+
+    // /**
+    //  * @return Z5600[] Returns an array of Z5600 objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('z')
+            ->andWhere('z.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('z.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Z5600
+    {
+        return $this->createQueryBuilder('z')
+            ->andWhere('z.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
