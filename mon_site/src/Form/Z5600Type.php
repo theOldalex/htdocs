e4ae57332a5f146334f7f378516b474c5e9bd671
theOldalex <alexandrediakitepro@gmail.com>
2021-10-26 17:02:24 +0200

    choices

--- mon_site/src/Form/Z5600Type.php
+++ mon_site/src/Form/Z5600Type.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
@@ -28,23 +29,55 @@ class Z5600Type extends AbstractType
                 'input_format'=> 'd/m/Y',
                 
             ])
-            ->add('livree', TextType::class
+            ->add('livree', ChoiceType::class, [
+                'choices' => [
+                    '--Choissisez une livrée--' => [
+                        'Transilien' => 'livree',
+                        'Ile-de-France' => 'livree',
+                        'Ile-de-France Mobilités' => 'livree',
+                        'Carmillon' => 'livree',
+
+                    ],
+
+                ],
                 
-                )
+            ])
+                
+                
             ->add('nombre_de_caisses', TextType::class
                 
                 )
-            ->add('stf', TextType::class
+            ->add('stf',ChoiceType::class, [
+                'choices' => [
+                    '--Choissisez une Supervision Technique de Flotte--' => [
+                        'STF Transilien ligne C (SLC)' => 'stf',
+                        'STF Transilien ligne D et R (SLD)' => 'stf',
+                        
+
+                    ],
+
+                ],
                 
-                )
+            ])
             ->add('radiation', DateType::class, [
                 'widget'=> 'single_text',
                 'input_format'=> 'd/m/Y',
                 
             ])
-            ->add('equipements_interieurs', TextType::class
+            ->add('equipements_interieurs',ChoiceType::class, [
+                'choices' => [
+                    '--Choissisez les équipements intérieurs--' => [
+                        'SIVE - vidéosurveillance' => 'equipements_interieurs',
+                        
+                        
+
+                    ],
+
+                ],
                 
-                )
+            ])
+                
+                
             ->add('lignes', TextType::class
                 
                 )
