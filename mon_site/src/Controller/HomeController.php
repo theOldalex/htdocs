f455a39443d14268b0417463a88d053fd175f708
theOldalex <alexandrediakitepro@gmail.com>
2021-12-14 12:08:09 +0100

    Modification des routes

--- mon_site/src/Controller/HomeController.php
+++ mon_site/src/Controller/HomeController.php
@@ -15,6 +15,6 @@ class HomeController extends AbstractController
     
     public function index(): Response
     {
-        return $this->render('home/index.html.twig'); 
+        return $this->render('base.html.twig'); 
     }
 }
