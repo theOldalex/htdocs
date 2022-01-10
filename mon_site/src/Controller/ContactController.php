64d6be269a65eba2099a1b0a5a425eda0bcdc0d0
theOldalex <alexandrediakitepro@gmail.com>
2022-01-10 14:07:52 +0100

    Ajout TemplatedEmail

--- mon_site/src/Controller/ContactController.php
+++ mon_site/src/Controller/ContactController.php
@@ -7,25 +7,28 @@ use App\Form\ContactType;
 
 
 use Symfony\Component\Mime\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(MailerInterface $mailer)
+    public function index(Request $request, MailerInterface $mailer): Response
     {
         $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
        
 
         if ($form->isSubmitted() && $form->isValid()) {
             $contact = $form->getData();
 
-            $email = (new Email())
-                ->from('a.diakite@yopmail.com')
-                ->to('91xwriter@contact.fr', 'no-reply')
+            $email = (new TemplatedEmail())
+                ->from($contact['email'])
+                ->to('91xwriter@contact.fr')
                 ->subject("What's the point of the Symfony Mailer, anyway?")
                 ->html('<h1>HTML email</h1><p>Email with HTML tags if the client supports it.</p>')
                 ->text(
@@ -36,7 +39,7 @@ class ContactController extends AbstractController
                 );
                 
             $mailer->send($email);
-            $this->addFlash('message', 'Votre message a été transmis, nous vous répondrons dans les meilleurs délais.');
+            $this->addFlash('message', 'Votre message a été transmis !');
         }
 
 
