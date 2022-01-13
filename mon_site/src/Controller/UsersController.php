2b826e11bbb1731a3af7a08f9284e201675c360a
theOldalex <alexandrediakitepro@gmail.com>
2022-01-13 17:23:02 +0100

    Mise à jour editpass

--- mon_site/src/Controller/UsersController.php
+++ mon_site/src/Controller/UsersController.php
@@ -3,14 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\EditProfileType;
 use App\Form\UserType;
+use App\Form\EditProfileType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 
 #[Route('/users/profil')]
@@ -27,7 +28,7 @@ class UsersController extends AbstractController
 
 
     #[Route('/{id}/edit', name: 'users_edit', methods: ['GET', 'POST'])]
-    
+
     public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(UserType::class, $user);
@@ -35,7 +36,7 @@ class UsersController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
-           
+
 
             return $this->redirectToRoute('users/profil', [], Response::HTTP_SEE_OTHER);
         }
@@ -46,12 +47,12 @@ class UsersController extends AbstractController
         ]);
     }
 
-     
+
     #[Route('/{id}', name: 'users_delete', methods: ['GET', 'POST'])]
-    
+
     public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
             $entityManager->remove($user);
             $entityManager->flush();
         }
@@ -59,12 +60,12 @@ class UsersController extends AbstractController
         return $this->redirectToRoute('users_delete', [], Response::HTTP_SEE_OTHER);
     }
 
-     
-     #[Route("/users/profil/edit", name:"profil_edit")]
-    
+
+    #[Route("/users/profil/edit", name: "profil_edit")]
+
     public function editProfile(Request $request)
     {
-        
+
         $user = $this->getUser();
         $form = $this->createForm(EditProfileType::class, $this->getUser());
         $form->handleRequest($request);
@@ -81,6 +82,27 @@ class UsersController extends AbstractController
         return $this->render('users/editprofile.html.twig', [
             'form' => $form->createView(),
         ]);
-}
+    }
 
+    #[Route("/users/pass/edit", name: "pass_edit")]
+    public function editPass(Request $request, UserPasswordHasherInterface $passwordencoder)
+    {
+        if($request->isMethod('POST')){
+            $em = $this->getDoctrine()->getManager();
+            $user = $this->getUser();
+
+            //On verifie ici si les deux mdp sont identiques avant d'encoder
+
+            if($request->request->get('pass') === $request->request->get('pass2')){
+$user->setPassword($passwordencoder->encodePassword($user, $request->request->get('pass')));
+
+            }else{
+                $this->addFlash('error', 'Les deux mots de passe ne sont pas identiques !');
+            }
+            
+        }
+        
+
+        return $this->render('users/editpass.html.twig');
+    }
 }
