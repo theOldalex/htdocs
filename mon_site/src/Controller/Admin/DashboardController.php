42606d50d17b5231b21f0cfcf565656cffcd7519
theOldalex <alexandrediakitepro@gmail.com>
2022-01-31 13:08:55 +0100

    Maj footer & front article.html.twig

--- mon_site/src/Controller/Admin/DashboardController.php
+++ mon_site/src/Controller/Admin/DashboardController.php
@@ -39,7 +39,7 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToUrl('Home', 'fa fa-home', "{{ path('admin') }}");
+        yield MenuItem::linkToUrl('Home', 'fa fa-home', '/');
         yield MenuItem::linkToCrud('Users', 'fa-solid fa fa-user', User::class);
         yield MenuItem::linkToCrud('Auteurs', 'fa-solid fa fa-user', Auteur::class);
         yield MenuItem::linkToCrud('Z5600', 'fa fa-train', Z5600::class);
