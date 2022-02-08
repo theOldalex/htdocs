9509dd55f3fd47a715045a812f4034acfa26ed9d
theOldalex <alexandrediakitepro@gmail.com>
2022-02-08 15:06:03 +0100

    Correction Contact Form

--- mon_site/src/Controller/ContactController.php
+++ mon_site/src/Controller/ContactController.php
@@ -2,12 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\ContactType;
-
-
-
-use Symfony\Component\Mime\Email;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,6 +18,8 @@ class ContactController extends AbstractController
     #[Route('/contact', name: 'contact')]
     public function index(Request $request, MailerInterface $mailer): Response
     {
+
+        
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
        
@@ -26,20 +27,19 @@ class ContactController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $contact = $form->getData();
 
-            $email = (new TemplatedEmail())
-                ->from($contact['email'])
+            $message = (new Email())
+                ->from($contact->get('email')->getData())
                 ->to('91xwriter@contact.fr')
-                ->subject("What's the point of the Symfony Mailer, anyway?")
-                ->html('<h1>HTML email</h1><p>Email with HTML tags if the client supports it.</p>')
-                ->text(
-                    $this->renderView(   
-                        'emails/contact.html.twig',
-                        compact('contact')
-                    ),
-                );
+                ->subject("Contact")
+                ->html([
+                    'email'=> $contact->get('email')->getData(),
+                     'message'=> $contact->get('message')->getData()
+                ]);
                 
-            $mailer->send($email);
+                
+            $mailer->send($message);
             $this->addFlash('message', 'Votre message a été transmis !');
+            return $this->redirectToRoute('home');
         }
 
 
