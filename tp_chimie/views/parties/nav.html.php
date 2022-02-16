de62424defdc8a9b799c5428dc1821a44d05f0db
Alexandre Diakité <85879945+theOldalex@users.noreply.github.com>
2022-02-16 10:56:34 +0100

    Delete tp_chimie directory

--- tp_chimie/views/parties/nav.html.php
+++
@@ -1,5 +0,0 @@
-<nav class="nav justify-content-center">
-	<a class="nav-link" href="<?= Helper::url('home') ?>">Accueil</a>
-	<a class="nav-link" href="<?= Helper::url('liste-molecules') ?>">Les molécules</a>
-	<a class="nav-link" href="<?= Helper::url('ajouter-molecules') ?>">Ajouter molécules</a>
-</nav>
