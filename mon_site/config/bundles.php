daa055ba8563d985b85ffd79108b91bc0917103f
theOldalex <alexandrediakitepro@gmail.com>
2022-03-03 13:36:31 +0100

    Installation du Bundle LiipImagine

--- mon_site/config/bundles.php
+++ mon_site/config/bundles.php
@@ -19,4 +19,5 @@ return [
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
